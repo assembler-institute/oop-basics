e75a5e2ba286b3c9588d6845efa654f44706c567
Iuliia Shikhanova <yshihanova@gmail.com>
2023-01-27 09:02:49 +0000

    base class done

--- animal-class.php
+++ animal-class.php
@@ -7,7 +7,8 @@ class animals {
     public $warmBlooded;
 
 
-function __construct(string $type, int $age, bool $vertebrates, bool $warmBlooded){
+function __construct(string $name, string $type, int $age, bool $vertebrates, bool $warmBlooded){
+    $this->$name=$name;
     $this->$type=$type; 
     $this->$age=$age;
     $this->$vertebrates=$vertebrates;
@@ -15,34 +16,49 @@ function __construct(string $type, int $age, bool $vertebrates, bool $warmBloode
 }
 
 
-function showAnimal(string $type, int $age, bool $vertebrates, bool $warmBlooded){
+function showAnimal(string $name, string $type, int $age, bool $vertebrates, bool $warmBlooded){
+   $subType = true; 
+   $blood = true; 
     if ($vertebrates === true){
-       $subType = "vertebrates";
-       return $subType;
+       $subType = "vertebrates";  
     }else{
-        $subType = "invertebrates";
-        return $subType; 
+        $subType = "invertebrates"; 
     } 
 
     if( $warmBlooded === true){ 
     $blood = "warm-blooded";
-    return $blood;
     }else{
     $blood = "cold-blooded";
-    return $blood;
     }
 
 echo "
-    <p><b>Animal</p></b><pre>
-    Type: $type,
-    Subtype: $subType,
-    Age: $age,
-    Group: $blood,
-    </pre><hr>";
+    <h1>$name</h1>
+    <b>Type:</b> $type,<br>
+    <b>Subtype:</b> $subType,<br>
+    <b>Age:</b> $age,<br>
+    <b>Group:</b> $blood,<br>";
 
+}
+
+function canBreath(string $name, string $breath){
+ echo "<p><b>How Do $name Breathe?</b></p> $breath";
+}
+
+function canMove(string $name, string $movement){
+    echo "<p><b>How Do $name move?</b></p> $movement";
+}
+
+function makeSound(string $name, string $sound){
+    echo "<p><b>Does $name make any sound?</b></p> $sound";
+}
+
+
+
+
+function __destruct(){
+    print "Destruct " . __CLASS__ . "\n";
 }
 }
 ?>
 
-/// How to get variable from if???
 
--- animal-object.php
+++ animal-object.php
@@ -1,9 +1,17 @@
 <?php
-require_once ("animals-class.php");
+require_once ("animal-class.php");
 
-$tiger = new animals("Mammal", 10, true, true);
+$tiger = new animals("Tiger", "Mammal", 10, true, true);
+$tiger->showAnimal("Tiger", "Mammal", 10, true, true);
+$tiger->canBreath("Tiger", "The respiratory system consists of the lungs. Just like humans and all mammals, Siberian tigers breathe through their nose and mouth into their lungs.");
+$tiger->canMove("Tiger", "They can run, jump, climb. The hind legs of the tiger are longer than their front legs. This characteristic enables them to leap forward distances up to 10 meters.");
+$tiger->makeSound("Tiger", "They growl, roar, chuff and moan.");
 
-$tiger->showAnimal("Mammal", 10, true, true);
 
+$crocodile = new animals("Crocodile", "Reptiles", 25, true, false);
+$crocodile->showAnimal("Crocodile", "Reptiles", 25, true, false);
+$crocodile->canBreath("Crocodile", "When basking on land with the mouth open, crocodilians breathe mostly through their mouth (the throat/palatal valve is open). When in water, the mouth is usually closed and they breathe mostly through their nostrils.");
+$crocodile->canMove("Crocodile", "The right front and left rear legs are lifted and moved forward while the front right and rear left legs, already on the ground, push backwards, propelling the crocodile's body forward.");
+$crocodile->makeSound("Crocodile", "Of all the reptiles, crocodiles are the most social and vocal. Adults hiss and bellow at each other. Babies chirp when they hatch.");
 
 ?>
