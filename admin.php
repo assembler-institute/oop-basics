3951d7200bee6faa4898c78c71d75b5578f71933
lucydct <tenalucia0@gmail.com>
2023-01-30 09:13:21 +0100

    solution

---
+++ admin.php
@@ -0,0 +1,56 @@
+<?php
+
+abstract class Device
+{
+
+    public $id;
+
+    public function __construct(int $id, string $brand, string $model, string $serialNumber)
+    {
+        $this->id = $id;
+        $this->brand = $brand;
+        $this->model = $model;
+        $this->serialNumber = $serialNumber;
+    }
+
+
+    public $brand;
+    public $model;
+    public $serialNumber;
+    public function getId()
+    {
+        return $this->model;
+    }
+
+    public function getSerialNumber()
+    {
+        return $this->serialNumber;
+    }
+
+    public function getDetail($device)
+    {
+        return $device->id;
+    }
+}
+
+class Mobile extends Device
+{
+
+    public $capacity;
+    public $price;
+
+    public function __construct(string $id, string $brand, string $model, string $serialNumber, string $capacity, string $price)
+    {
+        $this->capacity = $capacity;
+        $this->price = $price;
+        parent::__construct($id, $brand, $model, $serialNumber);
+    }
+
+    public function getDetail($device)
+    {
+        return Device::getDetail($device);
+    }
+}
+
+$mobile = new Mobile(5, 'Samsung', 'S20', 123456, '120GB', 1200);
+var_dump($mobile->getDetail($mobile));
